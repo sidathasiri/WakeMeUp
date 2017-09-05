88408ac305c0f36a1feffd9e71cc5b7d5726a51e
Sidath <sidath.14@cse.mrt.ac.lk>
2017-09-05 10:32:29 +0530

    added the map to home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+declare var google;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('map') mapElement
+  map: any;
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  ionViewDidLoad(){
+    this.initMap();
+  }
+
+  initMap(){
+    this.geolocation.getCurrentPosition().then((position) => {
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    }, (err) => {
+      console.log(err);
+    });
   }
 
 }
